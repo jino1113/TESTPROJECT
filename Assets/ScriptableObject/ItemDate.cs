78da39b5373caf0005a12dab26efd770c37a2033
jino1113 <155527950+jino1113@users.noreply.github.com>
2026-03-02 15:35:19 +0700

    Revert "Update Scriptableobject"
    This reverts commit a32b2cbb762533f62c894f99bf5c978ef1ea0676.

--- Assets/ScriptableObject/ItemDate.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewItem", menuName = "ScriptableObjects/ItemDate")]
-public class ItemDate : ScriptableObject
-{
-    public string itemName;
-    public int scoreValue;
-    public Color itemcolor;
-}
